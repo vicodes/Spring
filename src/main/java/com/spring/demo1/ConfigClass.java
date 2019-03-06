9f57178b7ea7e693566715f08bf5d61d262997ae
Mohit Singh <mohitsinghibm@gmail.com>
2019-03-06 16:11:38 +0530

    SOme New Lines added

--- src/main/java/com/spring/demo1/ConfigClass.java
+++ src/main/java/com/spring/demo1/ConfigClass.java
@@ -2,13 +2,14 @@ package com.spring.demo1;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 @Configuration
 //@ComponentScan(basePackages = {"com/spring/test1"})
-//@PropertySource("classpath:com/spring/test1/context.properties")
+@PropertySource("classpath:com/spring/test1/context.properties")
 //@ComponentScan("com.spring.test1")
 public class ConfigClass {
     @Bean
